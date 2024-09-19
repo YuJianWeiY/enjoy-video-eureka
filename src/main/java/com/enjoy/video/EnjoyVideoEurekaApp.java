cc24fc4f0b17aea688a60610d73b6d9ab015c0f2
YuJianWeiY <119394847+YuJianWeiY@users.noreply.github.com>
2024-09-19 19:23:51 +0800

    enjoy-video-eureka
    enjoy-video-eureka

--- src/main/java/com/enjoy/video/EnjoyVideoEurekaApp.java
+++ src/main/java/com/enjoy/video/EnjoyVideoEurekaApp.java
@@ -1,6 +1,5 @@
 package com.enjoy.video;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
